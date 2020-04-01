00e8057b442a5d6b446a89406b09324f64476ef8
NicolasFG <nicolas.figueroa@utec.edu.pe>
2020-04-01 17:54:18 -0500

    [Terminado]

---
+++ hand_wash/src/main/java/cs_utec/cs2901/App.java
@@ -0,0 +1,18 @@
+package cs_utec.cs2901;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        for(int i = 0; i <100; i++)
+        {
+          System.out.println( "Me debo lavar las manos por mas de 20 segundos" );
+        }
+    }
+}
